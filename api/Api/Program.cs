57955826f18daecb33be748bb7dee31a9ad921e0
MuhammedBahadi <muhammedbahadi925@gmail.com>
2025-09-30 09:58:53 +0300

    Feat(api) : Add connection string

--- api/Api/Program.cs
+++ api/Api/Program.cs
@@ -1,8 +1,17 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(ConnectionString));
 
 var app = builder.Build();
 
