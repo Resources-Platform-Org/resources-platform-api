a81546c9d23e7485f634db9abf0940ebe2324256
ALI <a711879040@gmail.com>
2025-10-01 16:33:05 +0300

    refactor(api): reorder properties in CourseProfessor for clarity and consistency

--- api/Core/Entites/CourseProfessor.cs
+++ api/Core/Entites/CourseProfessor.cs
@@ -1,8 +1,12 @@
 ﻿namespace Core.Entites;
+
 public class CourseProfessor
 {
+    // Foreign Keys [Composite Key]
+    // middle table to connect professors and courses M <=> M
     public int CourseID { get; set; }
-    public Course Course { get; set; } = default!;
     public int ProfessorID { get; set; }
+    // Navigation Properties
     public Professor Professor { get; set; } = default!;
+    public Course Course { get; set; } = default!;
 }
