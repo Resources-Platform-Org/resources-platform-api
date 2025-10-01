31f04240c1738279ec299d72f412f76df9185074
ALI <a711879040@gmail.com>
2025-10-01 21:08:47 +0300

    refact (core) : add commit [foregin key] to  major entity .

--- api/Core/Entites/Major.cs
+++ api/Core/Entites/Major.cs
@@ -3,6 +3,7 @@ public class Major
 {
     public int MajorID { get; set; }
     public string MajorName { get; set; } = string.Empty;
+    // Foreginkey :-
     public int UniversityID { get; set; }
     // Navigation Properties
     public University University { get; set; } = default!;
