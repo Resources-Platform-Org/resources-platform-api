7d197b15b18b9d81c98dcbd41953192531a02685
ALI BIN-SAMIDA <a711879040@gmail.com>
2025-10-17 16:23:06 +0300

    refactor (core): update User class properties to use UploadedFiles and remove Firstname and Lastname to Username

--- api/Core/Entities/User.cs
+++ api/Core/Entities/User.cs
@@ -2,11 +2,10 @@
 public class User
 {
     public int UserID { get; set; }
-    public string Firstname { get; set; }
-    public string Lastname { get; set; }
+    public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public enRole Role { get; set; } = enRole.User ;
     // Navigation Properties
-    public ICollection<File> Files { get; set; } = new List<File>();
+    public ICollection<Entities.File> UploadedFiles { get; set; } = new List<Entities.File>();
 }
