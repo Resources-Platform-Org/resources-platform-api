625985e24f7a0cf6dc58ae6f7fc9535b6cf534aa
ALI <A711879040@gmail.com>
2025-11-03 17:17:01 +0300

    fix(core): update ICourseRepository methods to return nullable Course type

--- api/Core/Interfaces/ICourseRepository.cs
+++ api/Core/Interfaces/ICourseRepository.cs
@@ -2,7 +2,7 @@ using Core.Entities;
 
 public interface ICourseRepository : IGenericRepository<Course>
 {
-    Task<Course> GetDetailsAsync(int courseId);
-    Task<Course> SearchAsync(object term);
+    Task<Course?> GetDetailsAsync(int courseId);
+    Task<Course?> SearchAsync(string term);
     
 }
