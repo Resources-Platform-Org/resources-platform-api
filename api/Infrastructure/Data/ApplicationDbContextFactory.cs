bf75ff429dc3d9ef08948e0e76c8533b02fe6506
ALI <A711879040@gmail.com>
2025-11-20 07:50:20 +0300

    feat (infrastructure) : Add ApplicationDbContextFactory for design-time support " This class implements IDesignTimeDbContextFactory<ApplicationDbContext> to facilitate design-time operations like migrations. "

---
+++ api/Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<
+                ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=.;Database=ResourceManagmentSystem;Trusted_Connection=True;TrustServerCertificate=true;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
