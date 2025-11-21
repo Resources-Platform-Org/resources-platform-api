d76a29072614c345e192e878d94c9308c6ca938a
ALI <A711879040@gmail.com>
2025-11-21 21:30:59 +0300

    feat(api): add LoginRequest and LoginResponse DTOs for user authentication

---
+++ api/Api/Dtos/Users/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Dtos.Users
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = "";
+        public string Username { get; set; } = "";
+        public string Role { get; set; } = "";
+    }
+
+}
