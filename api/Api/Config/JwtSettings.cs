c3bb79c7d4caba3277e775ff3f5be2676f8b4359
ALI <A711879040@gmail.com>
2025-11-21 21:27:21 +0300

    feat(api): add JwtSettings class for JWT configuration

---
+++ api/Api/Config/JwtSettings.cs
@@ -0,0 +1,7 @@
+public class JwtSettings
+{
+    public required string Key { get; set; }
+    public required string Issuer { get; set; }
+    public required string Audience { get; set; }
+    public int ExpiresInHours { get; set; }
+}
