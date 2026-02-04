8a04bce310c259be227bd0cd42d33d240ac0a3c3
ALI <a711879040@gmail.com>
2026-02-04 05:26:27 +0300

    Add MajorDto and MajorResponseDto classes for major data representation

---
+++ api/Api/Dtos/Majors/MajorResponseDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dtos.Majors
+{
+    public class MajorResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int UniversityId { get; set; }
+    }
+}
