1f023a8e1a17b469cfaf5d7959a28842002bcd6a
ALI <a711879040@gmail.com>
2026-02-04 07:31:59 +0300

    Add DocumentTypeDto and DocumentTypeResponseDto for document type handling

---
+++ api/Api/Dtos/DocumentTypes/DocumentTypeDto.cs
@@ -0,0 +1,8 @@
+
+namespace Api.Dtos.DocumentTypes
+{
+    public class DocumentTypeDto
+    {
+        public string TypeName { get; set; } = string.Empty;
+    }
+}
---
+++ api/Api/Dtos/DocumentTypes/DocumentTypeResponseDto.cs
@@ -0,0 +1,9 @@
+
+namespace Api.Dtos.DocumentTypes
+{
+    public class DocumentTypeResponseDto
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; } = string.Empty;
+    }
+}
