be874ab74231a31db39ffdf5a55f533ee659b943
ALI <a711879040@gmail.com>
2026-02-04 07:33:05 +0300

    Add DocumentType mappings to MappingProfile for improved data handling

--- api/Api/Mapping/MappingProfile.cs
+++ api/Api/Mapping/MappingProfile.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Core.Entities;
 using Api.Dtos.Universities;
 using Api.Dtos.Majors;
+using Api.Dtos.DocumentTypes;
 namespace Api.Mapping;
 
 public class MappingProfile : Profile
@@ -15,5 +16,12 @@ public class MappingProfile : Profile
         // Major Mappings :-
         CreateMap<Major, MajorResponseDto>();
         CreateMap<MajorDto, Major>();
+
+        // Document Type Mappings :-
+        CreateMap<DocumentType, DocumentTypeResponseDto>();
+        CreateMap<DocumentTypeDto, DocumentType>();
+
+        // Professor Mappings :-
+
     }
 }
