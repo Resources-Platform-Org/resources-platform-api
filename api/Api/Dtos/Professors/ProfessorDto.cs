e259c0d37a9c78d6de4580a8c640135590803589
ALI <a711879040@gmail.com>
2026-02-04 07:32:15 +0300

    Add ProfessorDto and ProfessorResponseDto for professor data handling

---
+++ api/Api/Dtos/Professors/ProfessorDto.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dtos.Professors
+{
+    public class ProfessorDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
