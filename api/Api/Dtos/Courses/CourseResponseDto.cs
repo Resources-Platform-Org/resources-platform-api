7e1d45ffb7146bc3233b396a4fb602de39946ed9
ALI <a711879040@gmail.com>
2026-02-08 11:38:35 +0300

    Add CourseResponseDto class to represent course response data structure

---
+++ api/Api/Dtos/Courses/CourseResponseDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Core.Entities;
+
+namespace Api.Dtos.Courses
+{
+    public class CourseResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public List<string>? Professors { get; set; } = new List<string>();
+        public List<string>? CourseMajors { get; set; } = new List<string>();
+        public int? FileCount { get; set; }
+    }
+}
