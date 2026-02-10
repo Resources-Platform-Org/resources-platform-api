15f50ee5dd4b85758f56696d8efc92177004d02c
ALI <a711879040@gmail.com>
2026-02-10 15:35:14 +0300

    refact : Remove FileServices implementation and IFileService interface

--- api/Api/Services/FileServices/IFileService.cs
+++
@@ -1,8 +0,0 @@
-namespace Api.Services.fileServices;
-
-public interface IFileService
-{
-    Task<string> UploadFile(IFormFile file, string folderName);
-    void DeleteFile(string fileName, string folderName);
-    string GetFilePath(string fileName, string folderName);
-}
