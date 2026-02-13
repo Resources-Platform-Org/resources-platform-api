c935e116cd809fd7c5adc7a59655d0d1f2c903f8
ALI <a711879040@gmail.com>
2026-02-13 07:59:38 +0300

    fix: Add UserImagesFolder property to FileSetting class

--- api/Core/Setting/FileSetting.cs
+++ api/Core/Setting/FileSetting.cs
@@ -2,6 +2,7 @@ namespace Core.Setting;
 
 public class FileSetting
 {
+    public string UserImagesFolder {  get; set; } = string.Empty;
     public int MaxFileSizeInMB { get; set; }
     public string AllowedExtensions { get; set; } = string.Empty;
     public string UploaderFolder { get; set; } = string.Empty;
