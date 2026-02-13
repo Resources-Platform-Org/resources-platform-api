31d39a6f24556ae33da8dd8d3a8c9ef54e253cdd
ALI <a711879040@gmail.com>
2026-02-13 07:59:30 +0300

    fix: Add nullable ProfilePicture property to User entity

--- api/Core/Entities/User.cs
+++ api/Core/Entities/User.cs
@@ -6,6 +6,7 @@ public class User
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+    public string? ProfilePicture { get; set; } = null;
     public enRoles Role { get; set; } = enRoles.User;
     // Navigation Properties
     public ICollection<Resource> UploadedResources { get; set; } = new List<Resource>();
