3ca9d3a085e798f6888b05945922792b94657f8f
ALI <a711879040@gmail.com>
2026-04-02 14:48:33 +0300

    feat: Add soft delete functionality with CreatedAt, UpdatedAt, DeletedAt, and IsDeleted fields to Users, Universities, Resources, Professors, Majors, DocumentTypes, and Courses

---
+++ api/Infrastructure/Migrations/20260402111825_addSoftDelWithInterceptors.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSoftDelWithInterceptors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Universities",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Universities",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Resources",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Resources",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Resources",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Resources",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Professors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Professors",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Majors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Majors",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "DocumentTypes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "DocumentTypes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Courses",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Universities");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Universities");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Professors");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Professors");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Majors");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Majors");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "DocumentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "DocumentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Courses");
+        }
+    }
+}
